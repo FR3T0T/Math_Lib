6848d5276d24aa863eb29bca4ef051dedc9269aa
FR3T0T <fr3t0t@gmail.com>
2025-04-04 17:17:55 +0200

    d

--- Adv_Math/ElektroMatBibTrinvis/@ElektroMatBibTrinvis/afslutForklaring.m
+++ Adv_Math/ElektroMatBibTrinvis/@ElektroMatBibTrinvis/afslutForklaring.m
@@ -1,17 +1,23 @@
-
-        
-        function forklaringsOutput = afslutForklaring(forklaringsOutput, resultat)
-            % AFSLUTFORKLARING Afslutter en forklaring med et resultat
-            %
-            % Syntax:
-            %   forklaringsOutput = ElektroMatBibTrinvis.afslutForklaring(forklaringsOutput, resultat)
-            %
-            % Input:
-            %   forklaringsOutput - Forklaringsoutput-struktur
-            %   resultat - Slutresultat (tekst eller symbolsk)
-            
-            if ischar(resultat)
-                forklaringsOutput.resultat = resultat;
-            else
-                forklaringsOutput.resultat = char(resultat);
-            end
+function forklaringsOutput = afslutForklaring(forklaringsOutput, resultat)
+    % AFSLUTFORKLARING Afslutter en forklaring med et resultat
+    %
+    % Syntax:
+    %   forklaringsOutput = ElektroMatBibTrinvis.afslutForklaring(forklaringsOutput, resultat)
+    %
+    % Input:
+    %   forklaringsOutput - Forklaringsoutput-struktur
+    %   resultat - Slutresultat (tekst eller symbolsk)
+    
+    if ischar(resultat)
+        forklaringsOutput.resultat = resultat;
+    else
+        forklaringsOutput.resultat = char(resultat);
+    end
+    
+    % Vis resultat
+    disp('RESULTAT:');
+    disp(['   ' forklaringsOutput.resultat]);
+    disp(' ');
+    disp(['===== AFSLUTTET: ' upper(forklaringsOutput.titel) ' =====']);
+    disp(' ');
+end
--- Adv_Math/ElektroMatBibTrinvis/@ElektroMatBibTrinvis/tilfoejTrin.m
+++ Adv_Math/ElektroMatBibTrinvis/@ElektroMatBibTrinvis/tilfoejTrin.m
@@ -1,18 +1,29 @@
-
-        
-        function forklaringsOutput = tilfoejTrin(forklaringsOutput, trinNummer, trinTitel, trinTekst, formel)
-            % TILFØJTRIN Tilføjer et forklaringstrin til forklaringsoutputtet
-            %
-            % Syntax:
-            %   forklaringsOutput = ElektroMatBibTrinvis.tilfoejTrin(forklaringsOutput, trinNummer, trinTitel, trinTekst, formel)
-            %
-            % Input:
-            %   forklaringsOutput - Forklaringsoutput-struktur
-            %   trinNummer - Nummer på trinet
-            %   trinTitel - Overskrift for trinet
-            %   trinTekst - Forklaringstekst for trinet
-            %   formel - (valgfri) Matematisk formel
-            
-            if nargin < 5
-                formel = '';
-            end
+function forklaringsOutput = tilfoejTrin(forklaringsOutput, trinNummer, trinTitel, trinTekst, formel)
+    % TILFØJTRIN Tilføjer et forklaringstrin til forklaringsoutputtet
+    %
+    % Syntax:
+    %   forklaringsOutput = ElektroMatBibTrinvis.tilfoejTrin(forklaringsOutput, trinNummer, trinTitel, trinTekst, formel)
+    %
+    % Input:
+    %   forklaringsOutput - Forklaringsoutput-struktur
+    %   trinNummer - Nummer på trinet
+    %   trinTitel - Overskrift for trinet
+    %   trinTekst - Forklaringstekst for trinet
+    %   formel - (valgfri) Matematisk formel
+    
+    if nargin < 5
+        formel = '';
+    end
+    
+    % Opret trin-struktur
+    nytTrin = struct('nummer', trinNummer, 'titel', trinTitel, 'tekst', trinTekst, 'formel', formel);
+    forklaringsOutput.trin{end+1} = nytTrin;
+    
+    % Vis trin
+    disp(['TRIN ' num2str(trinNummer) ': ' trinTitel]);
+    disp(trinTekst);
+    if ~isempty(formel)
+        disp(['   ' formel]);
+    end
+    disp(' ');
+end
